9518c915dd6d2f2a88e95ffd5e2f1f984fb2ec84
dlhdlh9969 <dlhdlh9969@gmail.com>
2024-01-08 09:42:33 +0900

    #	src/main/java/com/dlhdlh/controller/CustomerController.java

--- src/main/java/com/dlhdlh/controller/CustomerController.java
+++ src/main/java/com/dlhdlh/controller/CustomerController.java
@@ -44,7 +44,7 @@ public class CustomerController {
 		int maxPaging = 10;//페이징 최대 갯수
 		int maxRow = persetCust.getMaxrow(); //페이지당 최대 로우 갯수
 		
-		//업체명 검색 input이 null로 왔을 경우 where절에 like를 할 수 없으므로 빈값을 넣어줌
+		//업체명 검색 input이 null로 왔을 경우 where절에 like를 할 수 없으므로 빈값을 넣어줌!
 		String searchCustNm = customerDto.getSearchCustNm();
 		if(searchCustNm == null) {
 			searchCustNm = "";
@@ -66,6 +66,7 @@ public class CustomerController {
 				selectCustInfo = custList.getList().get(0);
 			}
 		}
+		selectCustInfo.setSearchCustNm(searchCustNm);
 		
 		mv.addObject("PersetCust", persetCust);
 		mv.addObject("CustList", custList);
